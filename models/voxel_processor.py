430f7b21218404a75d7f30da637942368b1cfb42
Ha-JH <hajeongho95@gmail.com>
2024-11-28 02:47:47 +0900

    downsample check

--- models/voxel_processor.py
+++ models/voxel_processor.py
@@ -12,11 +12,21 @@ class VoxelProcessor(nn.Module):
     def downscale(self, x):
         """Downscale voxels from input_size to target_size"""
         scale_factor = self.input_size // self.target_size
-        x = F.avg_pool3d(x.float(), kernel_size=scale_factor, stride=scale_factor)
+        x = F.max_pool3d(x.float(), kernel_size=scale_factor, stride=scale_factor)
+        return (x > 0.5).float()
+    
+    def downsample(self,x):
+        scale_factor = self.input_size // self.target_size
+        original_shape = x.shape
+        new_shape = original_shape[:-3]+(self.target_size, scale_factor, self.target_size, scale_factor, self.target_size, scale_factor)
+        x = x.reshape(new_shape)
+        x = x.any(dim=(-1)) 
+        x = x.any(dim=(-2)) 
+        x = x.any(dim=(-3)) 
         return (x > 0.5).float()
     
     def upscale(self, x):
         """Upscale voxels from target_size to input_size"""
-        x = F.interpolate(x, size=(self.input_size, self.input_size, self.input_size), 
+        x = F.interpolate(x, size=self.input_size, 
                          mode='trilinear', align_corners=False)
         return (x > 0.5).float()
